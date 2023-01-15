25c43cc0962d83f4ad6ec70ea00557a63a1bd170
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2023-01-15 23:22:34 +0600

    mongodb setup

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const port = process.env.port || 5000;
+console.log(process.env.MONGO_URL);
 app.listen(port, () => console.log(`Listening on port ${port}`));
