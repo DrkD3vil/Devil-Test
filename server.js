0a5e72569f9f3f96cb9f6c059fe303d133290f93
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2023-01-16 20:29:02 +0600

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const dbconfig = require("./config/dbconfig");
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Node Server Started at ${port}`));
