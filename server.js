ed65f7d073bdbbc90b90fbcd6d387ee1743b53d4
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2023-01-16 16:19:19 +0600

    mongo db connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const port = process.env.port || 5000;
-console.log(process.env.MONGO_URL);
-app.listen(port, () => console.log(`Listening on port ${port}`));
+require("dotenv").config();
+const dbconfig = require("./config/dbconfig");
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Node Server Started at ${port}`));
